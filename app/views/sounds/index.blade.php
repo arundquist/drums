5179aca67f687ae5420973e33493b5bd51f2f7ee
Andy Rundquist <andy.rundquist@gmail.com>
2016-07-05 11:26:17 -0500

    paginate all sounds

--- app/views/sounds/index.blade.php
+++ app/views/sounds/index.blade.php
@@ -1,1 +1,20 @@
-C:\xampp\htdocs\drums\app/views/sounds/index.blade.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rundquist Research crowdsourcing</title>
+
+ 
+    </head>
+    <body>
+        <table border ='1'>
+      @foreach($allsounds AS $sound)
+      <tr><td>{{$sound->freqeuncies}}</td>
+            <td>{{$sound->amplitudes}}</td>
+            <td>{{$sound->score}}</td>
+       </tr>
+      
+      @endforeach
+      </table>
+      {{$allsounds->links()}}
+    </body>
+</html>
